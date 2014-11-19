9cb7e05c16cdcdce930b97fe3c83c8b25270200c
tanasab <tanasab2@gmail.com>
2014-11-19 14:25:54 +0700

    Update order when confirm fail

--- ATOMs-API/confirm_order.php
+++ ATOMs-API/confirm_order.php
@@ -25,15 +25,15 @@ if(mysql_num_rows($user) == 0){
 
 $user = mysql_fetch_array($user);
 
-$query = 'SELECT * FROM `transaction` WHERE `user_id` = "'.$user['id'].'" AND `status` = 0 AND `bank` LIKE "'.$_GET['bank'].'" AND  `sms_time` >= ("'.$_GET['time'].'" - INTERVAL 1 MINUTE) AND  `sms_time` <= ("'.$_GET['time'].'" + INTERVAL 10 MINUTE) AND ROUND(`money`, 2) = "'.$_GET['money'].'" LIMIT 0, 1';
+$query = 'SELECT * FROM `order` WHERE `id` = "'.$_GET['order_id'].'" AND `user_id` = "'.$user['id'].'" AND `status` = "0"';
 $result = mysql_query($query);
 if(mysql_num_rows($result) > 0){
-	$transaction = mysql_fetch_array($result);
-	$query = 'SELECT * FROM `order` WHERE `id` = "'.$_GET['order_id'].'" AND `user_id` = "'.$user['id'].'" AND `status` = "0"';
+	$order = mysql_fetch_array($result);
+	$query = 'SELECT * FROM `transaction` WHERE `user_id` = "'.$user['id'].'" AND `status` = 0 AND `bank` LIKE "'.$_GET['bank'].'" AND  `sms_time` >= ("'.$_GET['time'].'" - INTERVAL 1 MINUTE) AND  `sms_time` <= ("'.$_GET['time'].'" + INTERVAL 10 MINUTE) AND ROUND(`money`, 2) = "'.$_GET['money'].'" LIMIT 0, 1';
 	$result = mysql_query($query);
 	if(mysql_num_rows($result) > 0){
-		$order = mysql_fetch_array($result);
-		if($order['money'] == $_GET['money']){
+		$transaction = mysql_fetch_array($result);
+		if(round($order['money'], 2) == round($_GET['money'], 2)){
 			mysql_query("UPDATE `android_app`.`transaction` SET `status` = '1' WHERE `transaction`.`id` = '".$transaction['id']."' LIMIT 1;");
 			mysql_query("UPDATE `android_app`.`order` SET `status` = '1', `bank` = '".$transaction['bank']."', `transaction_id` = '".$transaction['id']."', `transfered_at` = '".$transaction['sms_time']."' WHERE `order`.`id` = '".$order['id']."';");
 			$response["success"] = 1;
@@ -45,13 +45,22 @@ if(mysql_num_rows($result) > 0){
 		}
 	}
 	else{
-		$response["success"] = 0;
-		$response["message"] = "Error! We cannot find your order.";
+		//edit here plz.
+		if(round($_GET['money'], 2) == round($order['money'], 2)){
+			mysql_query("UPDATE `android_app`.`order` SET `bank` = '".$_GET['bank']."', `transfered_at` = '".$_GET['time']."' WHERE `order`.`id` = '".$order['id']."';");
+			$response["success"] = 0;
+			$response["message"] = "Sorry, we cannot find your transaction. Please try again or wait for 30 minutes.";
+		}
+		else{
+			$response["success"] = 0;
+			$response["message"] = "Error! Transfer amount mismatch.";
+		}
+
 	}
 }
 else{
 	$response["success"] = 0;
-	$response["message"] = "Error! Sorry, we cannot find your transaction. Please try again in 30 minutes. || ".$query;
+	$response["message"] = "Error! We cannot find your order.";
 }
 
 echo json_encode($response);
--- ATOMs-API/sms_receiver.php
+++ ATOMs-API/sms_receiver.php
@@ -21,8 +21,8 @@ if(mysql_num_rows($result) == 0)
 	exit();
 }
 
-$result 		= mysql_fetch_array($result);
-$user_id 		= $result['id'];
+$user 			= mysql_fetch_array($result);
+$user_id 		= $user['id'];
 $sender 		= $_POST['sender'];
 $receiver 		= "";
 $received_at 	= $_POST['time'];
@@ -43,13 +43,14 @@ if ($result)
 	// successfully inserted into database
 	$response["success"] = 1;
 	$response["message"] = "SMS successfully receive.";
-	echo json_encode($response);
+
 
 	//Process transaction
 	require_once('transaction_processor.php');
 	$query 		= sprintf('SELECT * FROM `sms_income` WHERE `user_id` = %d AND `sender` = "%s" AND `receiver` = "%s" AND `message` = "%s" AND `received_at` = "%s"',$user_id,addslashes($sender),addslashes($receiver),addslashes($message),$received_at);
 	$row = mysql_fetch_array(mysql_query($query));
 	$process = transaction_process($row['id']);
+	$response['process'] = $process;
 	if($process['success'] == 1)
 	{
 		$query 	= sprintf('SELECT * FROM `order` WHERE `user_id` = %d AND (`transfered_at` >= ("%s" - INTERVAL 1 MINUTE ) AND `transfered_at` <= ("%s" + INTERVAL 1 MINUTE ) AND ABS(%f-`money`) <= 0.00001 AND `status` = 0 AND transaction_id IS NULL AND `bank`= "%s") LIMIT 1', $user_id, $process['time'], $process['time'], $process['money'], $process['bank']);
@@ -60,10 +61,21 @@ if ($result)
 			$query 	= sprintf('UPDATE `order` SET `status`= 1 ,`transaction_id`= %d WHERE `id` = %d', $process['transaction_id'], $order['id']);
 			mysql_query($query);
 			mysql_query('UPDATE `transaction` SET `status`=1 WHERE `id` = '.$process['transaction_id']);
-
+			try
+			{
+				$request = file_get_contents($user['url_script']."?atoms_token=".sha1($user['server_token'])."&order_id=".$order['id']."&status=1");
+				if($request !== "OK")
+				{
+					$response["message"] = "Update ATOMs database success but fail to update data in your server.";
+				}
+			}
+			catch( Exception $e )
+			{
+				$response["message"] = "Update ATOMs database success but fail to update data in your server.";
+			}
 		}
-
 	}
+	echo json_encode($response);
 }
 else
 {
